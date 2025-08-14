f0a4075872f6e4eda1f09aa13414c922ad393f56
Ninja 903 <rushabhshah658@gmail.com>
2025-08-14 21:45:32 +0530

    changes

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -3,11 +3,14 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    DB_PASSWORD: z.string().min(1),
-    DB_USER: z.string().min(1),
-    DB_HOST: z.string().min(1),
-    DB_PORT: z.string().min(1),
-    DB_NAME: z.string().min(1),
+    DATABASE_URL: z.string().url().optional(),
+
+    DB_USER: z.string().min(1).optional(),
+    DB_PASSWORD: z.string().min(1).optional(),
+    DB_HOST: z.string().min(1).optional(),
+    DB_PORT: z.string().min(1).optional(),
+    DB_NAME: z.string().min(1).optional(),
+
     CLERK_SECRET_KEY: z.string().min(1),
     CLERK_WEBHOOK_SECRET: z.string().min(1),
     UPLOADTHING_TOKEN: z.string().min(1),
@@ -16,15 +19,15 @@ export const env = createEnv({
     RESEND_API_KEY: z.string().min(1),
     SERVER_URL: z.string().min(1),
   },
-  createFinalSchema: env => {
-    return z.object(env).transform(val => {
-      const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER, ...rest } = val
-
-      return {
-        ...rest,
-        DATABASE_URL: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-      }
-    })
+  createFinalSchema: schema => {
+    return z
+      .object(schema)
+      .transform((values) => {
+        if (!values.DATABASE_URL && values.DB_USER && values.DB_PASSWORD && values.DB_HOST && values.DB_PORT && values.DB_NAME) {
+          values.DATABASE_URL = `postgres://${encodeURIComponent(values.DB_USER)}:${encodeURIComponent(values.DB_PASSWORD)}@${values.DB_HOST}:${values.DB_PORT}/${values.DB_NAME}`
+        }
+        return values
+      })
   },
   emptyStringAsUndefined: true,
   experimental__runtimeEnv: process.env,
