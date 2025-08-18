56d5ddb50b1a78981978fee0fcab2fb70dff7ad4
Ninja 903 <rushabhshah658@gmail.com>
2025-08-18 23:08:51 +0530

    changes

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,8 +1,18 @@
 
-export * from "@/drizzle/schema/user"
-export * from "./schema/organization"
-export * from "./schema/jobListing"
-export * from "./schema/userResume"
-export * from "./schema/userNotificationSettings"
-export * from "./schema/jobListingApplication"
-export * from "./schema/organizationUserSettings"
+import * as userSchema from "./schema/user";
+import * as organizationSchema from "./schema/organization";
+import * as jobListingSchema from "./schema/jobListing";
+import * as userResumeSchema from "./schema/userResume";
+import * as userNotificationSettingsSchema from "./schema/userNotificationSettings";
+import * as jobListingApplicationSchema from "./schema/jobListingApplication";
+import * as organizationUserSettingsSchema from "./schema/organizationUserSettings";
+
+export const schema = {
+  ...userSchema,
+  ...organizationSchema,
+  ...jobListingSchema,
+  ...userResumeSchema,
+  ...userNotificationSettingsSchema,
+  ...jobListingApplicationSchema,
+  ...organizationUserSettingsSchema,
+};
